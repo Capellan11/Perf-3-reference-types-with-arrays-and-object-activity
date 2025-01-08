92ed184a7228d1be36dc495e92a96f1f0b3299e9
Capellan11 <aureocapellan11@gmail.com>
2025-01-08 20:53:00 +0800

    Create arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,13 @@
+const favSingers = ["taylor swift", "flow g", "justin bieber"];
+
+console.log(favSingers[0]);
+
+const favNumbers = [7, 13, 21, 37];
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]); 
+console.log(mixedArr[1]); 
+console.log(mixedArr[1][0]); 
+console.log(mixedArr[2]); 
+console.log(mixedArr[3]);
